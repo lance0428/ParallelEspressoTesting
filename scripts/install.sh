e5b384f764e51cd73285b80f2102051d538f7d95
Lance Johnson <lance.johnson@betterup.co>
2019-01-10 00:07:28 -0600

    fix all script for sharding approach

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,21 @@
-adb uninstall com.ldj.parallelespressotesting
-adb uninstall com.ldj.parallelespressotesting.test
-adb install app/build/outputs/apk/debug/app-debug.apk
-adb install app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
+#!/bin/bash
+
+# prepare devices
+devices=()
+for device in $(adb devices | grep -v List | grep device)
+do
+    if [ $device == "device" ]; then
+        continue
+    fi
+    devices+=("$device")
+done
+
+for index in "${!devices[@]}"
+do
+    device=${devices[$index]}
+    echo "Setting up device = $device"
+    adb -s $device uninstall com.ldj.parallelespressotesting
+    adb -s $device uninstall com.ldj.parallelespressotesting.test
+    adb -s $device install app/build/outputs/apk/debug/app-debug.apk
+    adb -s $device install app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
+done
--- scripts/raw.sh
+++ scripts/raw.sh
@@ -3,6 +3,14 @@
 . scripts/config.sh
 rawTests=$1
 
-echo "Running test on package $package w/ runner $testInstrumentationRunner and redirecting to $rawTests"
+# prepare devices
+devices=()
+for device in $(adb devices | grep -v List | grep device)
+do
+    if [ $device == "device" ]; then
+        continue
+    fi
+    devices+=("$device")
+done
 
-adb shell am instrument -w -r -e log true $package.test/$testInstrumentationRunner > $rawTests
+adb -s ${devices[0]} shell am instrument -w -r -e log true $package.test/$testInstrumentationRunner > $rawTests
